d437ffa818137096b1d5d678f74919909e1ed50b
Alexander2184 <97836593+Alexander2184@users.noreply.github.com>
2024-07-02 14:47:10 +0300

    Update key_generation_sandbox.py
    fixed bug with non-found circles, improved dots search

--- key_generation_sandbox/key_generation_sandbox.py
+++ key_generation_sandbox/key_generation_sandbox.py
@@ -19,11 +19,15 @@ def process_file(input_file):
     req_size = (req_width, req_length)
 
     input_image = Image.open(os.path.join(input_folder, input_file))
+    saved_image = input_image.copy()
+
     log_picture_number = 0
+
     def new_log_picture():
         nonlocal log_picture_number
         log_picture_number += 1
         return os.path.join(output_folder, filename + "_" + str(log_picture_number) + ".png")
+
     last_picture = os.path.join(output_folder, "_last_" + filename + ".png")
 
     width, height = input_image.width, input_image.height
@@ -31,7 +35,6 @@ def process_file(input_file):
     #to_gray
     input_image = return_grayscale(input_image, width, height)
 
-    saved_gray_image = input_image.copy()
 
     input_image.save(new_log_picture())
 
@@ -43,12 +46,11 @@ def process_file(input_file):
 
     input_pixels = input_image.load()
 
-
     # Find circles, assuming their D is at least half of min(h, w)
-    rmin = min(input_image.height, input_image.width)//4
-    rmax = min(input_image.height, input_image.width)//2
+    rmin = min(input_image.height, input_image.width) // 4
+    rmax = (int)(min(input_image.height, input_image.width) / 1.9)
     steps = 100
-    threshold = 0.2
+    threshold = 0.45
 
     points = []
     for r in range(rmin, rmax + 1):
@@ -74,13 +76,18 @@ def process_file(input_file):
         return
 
     x0, y0, r0 = 0, 0, 0
+    max_overflow = 15
+
+    def check_inside(x, y, w, h, overflow=0, rd=0):
+        return (((x - rd) > -overflow) & ((y - rd) > -overflow)) & (
+                    ((x + rd) < (w + overflow)) & ((y + rd) < (h + overflow)))
 
     # looking for the largest circle fitting inside image
     for circle in circles:
         x = circle[0]
         y = circle[1]
         r = circle[2]
-        if ((r > r0) & (((x - r) > 0) & ((y - r) > 0))) & (((x + r) < width) & ((y + r) < height)):
+        if (r > r0) & check_inside(x, y, width, height, max_overflow, r):
             x0, y0, r0 = x, y, r
 
     if r0 == 0:
@@ -89,34 +96,48 @@ def process_file(input_file):
 
     #extrapolating found circle to original scale
     x0, y0, r0 = x0 * compression_power + compression_power // 2, y0 * compression_power + compression_power // 2, r0 * compression_power - compression_power // 2
-    saved_gray_pixels = saved_gray_image.load()
+    width = saved_image.width
+    height = saved_image.height
+    saved_pixels = saved_image.load()
 
-    inner_colours = []
+    inner_colours = [[], [], []]
     outer_colours = []
 
-    for x1 in range(-r0, r0+1):
-        for y1 in range(-r0, r0+1):
-            if (x1) ** 2 + (y1)**2 <= r0**2:
-                inner_colours.append(saved_gray_pixels[x0 + x1, y0 + y1][0])
+    for x1 in range(-r0, r0 + 1):
+        for y1 in range(-r0, r0 + 1):
+            if ((x1) ** 2 + (y1) ** 2 <= r0 ** 2) & check_inside(x0 + x1, y0 + y1, width, height):
+                inner_colours[0].append(saved_pixels[x0 + x1, y0 + y1][0])
+                inner_colours[1].append(saved_pixels[x0 + x1, y0 + y1][1])
+                inner_colours[2].append(saved_pixels[x0 + x1, y0 + y1][2])
 
-    inner_colours.sort()
+    inner_colours[0].sort()
+    inner_colours[1].sort()
+    inner_colours[2].sort()
 
-    median = inner_colours[len(inner_colours) // 2]
-    circled_image = Image.new("RGB", [2*r0+1, 2*r0+1])
+    median = (inner_colours[0][len(inner_colours[0]) // 2],
+              inner_colours[1][len(inner_colours[1]) // 2],
+              inner_colours[2][len(inner_colours[2]) // 2])
+    circled_image = Image.new("RGB", [2 * r0 + 1, 2 * r0 + 1])
     circled_image.paste(input_image)
     draw_result = ImageDraw.Draw(circled_image)
 
     #redrawing image to make circle big and erase blackground
-    for x1 in range(-r0, r0+1):
-        for y1 in range(-r0, r0+1):
-            dist = sqrt((x1 ** 2 + y1 **2) / (r0 ** 2))
-            if dist < 0.98:
-                draw_result.point((x1 + r0, y1 + r0), saved_gray_pixels[x1 + x0, y1 + y0])
-            elif dist >= 1:
-                draw_result.point((x1 + r0, y1 + r0), (median, median, median))
-            else:
-                color = (int)(saved_gray_pixels[x1 + x0, y1 + y0][0] * (50.0 - 50 * dist) + median * (50.0 * dist - 49))
-                draw_result.point((x1 + r0, y1 + r0),  (color, color, color))
+    for x1 in range(-r0, r0 + 1):
+        for y1 in range(-r0, r0 + 1):
+            dist = sqrt((x1 ** 2 + y1 ** 2) / (r0 ** 2))
+            if check_inside(x1 + x0, y1 + y0, width, height):
+                color = (abs(median[0] - saved_pixels[x1 + x0, y1 + y0][0]) +
+                         abs(median[1] - saved_pixels[x1 + x0, y1 + y0][1]) +
+                         abs(median[2] - saved_pixels[x1 + x0, y1 + y0][2]))
+                if dist >= 1:
+                    draw_result.point((x1 + r0, y1 + r0), black)
+                elif (dist < 0.98):
+                    draw_result.point((x1 + r0, y1 + r0), (color, color, color))
+                else:
+                    color = int(color * (50.0 - 50 * dist))
+                    draw_result.point((x1 + r0, y1 + r0), (color, color, color))
+
+
 
     circled_image.save(new_log_picture())
 
@@ -124,21 +145,20 @@ def process_file(input_file):
     circled_image = circled_image.resize(req_size)
     circled_image.save(new_log_picture())
 
-
     #making deviant-colored points black, other - white
     binary_image = Image.new("RGB", req_size)
     output_pixels = circled_image.load()
 
     draw_result = ImageDraw.Draw(binary_image)
 
-    pixel_differ = 25
+    pixel_differ = 60
 
     for x1 in range(req_width):
         for y1 in range(req_length):
-            if abs(output_pixels[x1, y1][0] - median) > pixel_differ:
-                draw_result.point((x1, y1), black)
-            else:
+            if output_pixels[x1, y1][0] > pixel_differ:
                 draw_result.point((x1, y1), white)
+            else:
+                draw_result.point((x1, y1), black)
 
     binary_image.save(new_log_picture())
     binary_image.save(last_picture)

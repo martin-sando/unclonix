3fc61435da5a1022212537ff5d2fd73fa602bb66
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2024-09-26 23:11:58 +0100

    Support --reverse

--- key_generation_sandbox/key_generation_sandbox.py
+++ key_generation_sandbox/key_generation_sandbox.py
@@ -7,13 +7,17 @@ import utils
 input_folder, output_folder, bloblist_folder, report_folder, time_folder = utils.input_folder, utils.output_folder, utils.bloblist_folder, utils.report_folder, utils.time_folder
 
 def run_all(prefix, mask):
-    input_files = os.listdir(input_folder)
-    for input_file in sorted(input_files)[::-1]:
+    input_files = sorted(os.listdir(input_folder))
+    if reverse:
+        input_files = input_files[::-1]
+    for input_file in input_files:
         if '~' in input_file or mask not in input_file or not input_file.startswith(prefix):
             continue
         filename = input_file.split('.')[0]
         if not os.path.isfile(os.path.join(bloblist_folder, filename + '.txt')):
             image_processing.process_photo(input_file, False)
+        else:
+            print("Reusing computed bloblist")
         bloblist_operations.process_photo(input_file, True)
 
 
@@ -25,9 +29,12 @@ if __name__ == '__main__':
     utils.set_total_time()
     prefix = ''
     mask = ''
+    reverse = False
     for arg in sys.argv:
         if arg.startswith('--prefix='):
             prefix = arg[9:]
         if arg.startswith('--mask='):
             mask = arg[7:]
-    run_all(prefix, mask)
+        if arg.startswith('--reverse'):
+            reverse = True
+    run_all(prefix, mask, reverse)

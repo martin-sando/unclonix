a885b9769f78da5562507bc4427661b21e878f0f
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2024-10-22 13:18:16 +0100

    minor

--- key_generation_sandbox/bot.py
+++ key_generation_sandbox/bot.py
@@ -17,17 +17,17 @@ def manual(message):
 @bot.message_handler(content_types=['photo'])
 def handle_photo(message):
     print('message.photo =', message.photo)
-    fileID = message.photo[-1].file_id
-    print('fileID =', fileID)
-    file_info = bot.get_file(fileID)
+    file_id = message.photo[-1].file_id
+    print('file_id =', file_id)
+    file_info = bot.get_file(file_id)
     print('file.file_path =', file_info.file_path)
     downloaded_file = bot.download_file(file_info.file_path)
     name = "bot_photo" + str(hash(datetime.datetime.now())) + ".jpg"
     with open("../input/" + name, 'wb') as new_file:
         new_file.write(downloaded_file)
     given_hash = key_generation_sandbox.handle_image(name)
-    if (given_hash == "Error"):
-        bot.send_message(message.from_user.id, "An error occured while calculating hash")
+    if given_hash == "Error":
+        bot.send_message(message.from_user.id, "An error occurred while calculating hash")
     else:
         bot.send_message(message.from_user.id, "Image hash is " + given_hash)
 

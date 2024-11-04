8cb81435c8518bae273de8179d03deb3bf000684
Alex <sonkot25@mail.ru>
2024-11-05 00:32:19 +0300

    bugfix

--- key_generation_sandbox/bot.py
+++ key_generation_sandbox/bot.py
@@ -17,14 +17,34 @@ def start(message):
 
 @bot.message_handler(commands=['help'])
 def manual(message):
-    bot.send_message(message.from_user.id, "This is a bot to convert Unclonix label images to hash.\nRight now it supports only photos and converts them to hash.\nUnclonix website: https://unclonix.com/")
+    bot.send_message(message.from_user.id, "This is a bot to convert Unclonix label images to hash.\nRight now it "
+                                           "supports only photos and converts them to hash.\nUnclonix website: "
+                                           "https://unclonix.com/")
 
 
 
-@bot.message_handler(content_types=['photo', 'document'])
+@bot.message_handler(content_types=['photo'])
 def handle_photo(message):
     print('message.photo =', message.photo)
     file_id = message.photo[-1].file_id
+    print('file_id =', file_id)
+    file_info = bot.get_file(file_id)
+    print('file.file_path =', file_info.file_path)
+    downloaded_file = bot.download_file(file_info.file_path)
+    name = "bot" + datetime.datetime.now(datetime.UTC).strftime('%Y%m%d_%H%M%S%f') + ".jpg"
+    with open("../input/" + name, 'wb') as new_file:
+        new_file.write(downloaded_file)
+    utils.set_total_time()
+    try:
+        the_hash = key_generation_sandbox.run(name)
+        bot.send_message(message.from_user.id, "Image hash is " + the_hash)
+    except:
+        print(traceback.format_exc())
+
+@bot.message_handler(content_types=['document'])
+def handle_photo(message):
+    print('message.photo =', message.photo)
+    file_id = message.document.file_id
     print('file_id =', file_id)
     file_info = bot.get_file(file_id)
     print('file.file_path =', file_info.file_path)
